13492ad9f2d02ec160c6334f103df70c577000f4
terry <kdjtiger@gmail.com>
2023-05-03 11:23:13 +0900

    20230503

---
+++ terrydkim/Week09/[P] 124 나라의 숫자.py
@@ -0,0 +1,14 @@
+from collections import deque
+def solution(n):
+    answer = deque()
+    while n:
+        a = n % 3
+        if a == 0:
+            n -= 1
+            answer.append('4')
+        else:
+            answer.appendleft(str(a))
+        n //= 3
+    return ''.join(answer)
+
+print(solution(9))
